3f19405015a1b704a0ca22326d40ff0bcbe54462
Tumer Altunbas <tumeraltunbas1903@gmail.com>
2020-12-30 22:02:05 +0300

    Add files via upload

---
+++ birth_year_age.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main () {
+	int birthyear;
+	int age;
+	printf("Please enter your year of birth: ");
+	scanf("%d",&birthyear);
+	age=2021-birthyear;
+	printf("Your year of birth: %d",age);
+	
+	
+	return 0;
+}
